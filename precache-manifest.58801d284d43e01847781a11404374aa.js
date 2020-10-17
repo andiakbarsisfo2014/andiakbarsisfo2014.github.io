54263c77a58d6df17a5f13b7b871bce9ab45ee08
Akbar <60900114063@uin-alauddin.ac.id>
2020-10-17 19:38:17 +0800

    S

---
+++ precache-manifest.58801d284d43e01847781a11404374aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a7ff040a4f0de41f86c96d52a3ba870",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c8246d449c96adc6e84",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "615fc15297ce32987528",
+    "url": "/static/css/main.69df538c.chunk.css"
+  },
+  {
+    "revision": "5c8246d449c96adc6e84",
+    "url": "/static/js/2.0d859ad9.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.0d859ad9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "615fc15297ce32987528",
+    "url": "/static/js/main.388f7e6a.chunk.js"
+  },
+  {
+    "revision": "ccaefecb50f02e1fdd6c",
+    "url": "/static/js/runtime-main.6a369087.js"
+  }
+]);
